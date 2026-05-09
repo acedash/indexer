c7d24e97c090dcb7388ee9c96809595fe14ebd34
acedash <mailtokhanasrar@gmail.com>
2026-05-09 18:28:40 +0530

    Pass empty options object to Prisma constructor

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,5 +1,6 @@
 import { PrismaClient } from '@prisma/client';
 
-const prisma = new PrismaClient();
+// Prisma 7 requires at least an empty object to initialize correctly on some environments
+const prisma = new PrismaClient({});
 
 export default prisma;
